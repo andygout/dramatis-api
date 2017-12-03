e7b69372d1a559b5afbe4ff676d9f245e44d50a8
Andy Gout <andygout@users.noreply.github.com>
2017-12-03 18:07:37 +0000

    Use: neo4j-driver package (#12)

---
+++ server/database/convert-records-to-objects.js
@@ -0,0 +1,31 @@
+import { v1 as neo4j } from 'neo4j-driver';
+
+export default response => {
+
+	let records = response.records || [];
+
+	records = records.map(record => {
+
+		const object = {};
+
+		record.keys.forEach((key, index) => {
+
+			object[key] = record._fields[index];
+
+			if (neo4j.isInt(object[key])) {
+
+				const neoInt = neo4j.int(object[key]);
+
+				if (neoInt.inSafeRange()) object[key] = neoInt.toNumber();
+
+			}
+
+		});
+
+		return object;
+
+	});
+
+	return records;
+
+};
