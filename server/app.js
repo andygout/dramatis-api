04e476f2563f05b99ca8a10ce322943b7af0cd61
Andy Gout <andygout@users.noreply.github.com>
2019-04-27 22:08:39 +0100

    Lowercase: content-type response header (#18)

--- server/app.js
+++ server/app.js
@@ -22,7 +22,7 @@ app.use(logger('dev'));
 app.use((req, res, next) => {
 
 	res.header('Access-Control-Allow-Origin', '*');
-	res.header('Access-Control-Allow-Headers', 'Content-Type');
+	res.header('Access-Control-Allow-Headers', 'content-type');
 
 	next();
 
