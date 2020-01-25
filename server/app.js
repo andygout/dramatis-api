bbd38afc8b02d79789b1f2587bcf298f4174c95b
Andy Gout <andygout@users.noreply.github.com>
2020-01-25 15:48:30 +0000

    Remove obsolete dependencies (core-js + regenerator-runtime) (#68)
    * Remove core-js dependency
    
    * Remove regenerator-runtime dependency

--- server/app.js
+++ server/app.js
@@ -3,9 +3,6 @@
 	no-unused-vars: ["error", { "argsIgnorePattern": "next" }]
 */
 
-import 'core-js/stable';
-import 'regenerator-runtime/runtime';
-
 import './dotenv';
 import bodyParser from 'body-parser';
 import express from 'express';
