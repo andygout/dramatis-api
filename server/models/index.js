3c1831469c93f1c958aedbebcb2fde5ee6cf7874
Andy Gout <andygout@users.noreply.github.com>
2020-03-29 17:04:19 +0100

    Exclude Base from models index (#80)

--- server/models/index.js
+++ server/models/index.js
@@ -1,4 +1,3 @@
-import Base from './base';
 import BasicModel from './basic-model';
 import Character from './character';
 import PersonCastMember from './person-cast-member';
@@ -9,7 +8,6 @@ import Role from './role';
 import Theatre from './theatre';
 
 export {
-	Base,
 	BasicModel,
 	Character,
 	PersonCastMember,
