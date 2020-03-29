f0f233859f4055e9a7568dc31e24dd39f62d7c9c
andygout <andygout@hotmail.co.uk>
2020-03-29 16:55:00 +0100

    Import models from directory not files

--- test-unit/server/controllers/theatres.test.js
+++ test-unit/server/controllers/theatres.test.js
@@ -2,7 +2,7 @@ import { expect } from 'chai';
 import proxyquire from 'proxyquire';
 import sinon from 'sinon';
 
-import Theatre from '../../../server/models/theatre';
+import { Theatre } from '../../../server/models';
 
 describe('Theatres controller', () => {
 
