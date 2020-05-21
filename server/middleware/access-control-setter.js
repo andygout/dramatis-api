cd7a2514e76c3bad5419644e1a18e0342e35686a
andygout <andygout@hotmail.co.uk>
2020-05-21 20:49:11 +0100

    Add response header Access-Control-Allow-Methods

--- server/middleware/access-control-setter.js
+++ server/middleware/access-control-setter.js
@@ -3,6 +3,14 @@ export default function (request, response, next) {
 	response.header('Access-Control-Allow-Origin', '*');
 	response.header('Access-Control-Allow-Headers', 'content-type');
 
+	if (request.method === 'OPTIONS') {
+
+		response.header('Access-Control-Allow-Methods', 'PUT');
+
+		return response.sendStatus(200);
+
+	}
+
 	next();
 
 }
