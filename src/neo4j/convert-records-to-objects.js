9514fc2399c4cf46e538c945de05cc59df088455
Andy Gout <andygout@users.noreply.github.com>
2020-06-20 21:41:06 +0100

    Extract Neo4j convertRecordsToObjects to module (#122)

---
+++ src/neo4j/convert-records-to-objects.js
@@ -0,0 +1,31 @@
+import neo4j from 'neo4j-driver';
+
+export default response => {
+
+	let records = response.records || [];
+
+	records = records.map(record => {
+
+		const object = {};
+
+		record.keys.forEach((key, index) => {
+
+			object[key] = record._fields[index];
+
+			if (neo4j.isInt(object[key])) {
+
+				const neo4jInt = neo4j.int(object[key]);
+
+				if (neo4jInt.inSafeRange()) object[key] = neo4jInt.toNumber();
+
+			}
+
+		});
+
+		return object;
+
+	});
+
+	return records;
+
+};
