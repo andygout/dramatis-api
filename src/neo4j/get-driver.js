b265e1177dd2d51f0619da6dfe56fbbd0f2c0cd7
Andy Gout <andygout@users.noreply.github.com>
2020-06-22 21:50:00 +0100

    Reorder NODE_ENV ternary (#127)

--- src/neo4j/get-driver.js
+++ src/neo4j/get-driver.js
@@ -2,8 +2,8 @@ import neo4j from 'neo4j-driver';
 
 const { DATABASE_URL, DATABASE_USERNAME, DATABASE_PASSWORD, NODE_ENV } = process.env;
 
-const driver = (NODE_ENV !== 'unit-test') ?
-	neo4j.driver(DATABASE_URL, neo4j.auth.basic(DATABASE_USERNAME, DATABASE_PASSWORD)) :
-	null;
+const driver = (NODE_ENV === 'unit-test') ?
+	null :
+	neo4j.driver(DATABASE_URL, neo4j.auth.basic(DATABASE_USERNAME, DATABASE_PASSWORD));
 
 export const getDriver = () => driver;
