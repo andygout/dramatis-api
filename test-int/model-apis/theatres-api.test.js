4db5bbdbb67778b937c6762b6403932594cf1fc3
andygout <andygout@hotmail.co.uk>
2020-07-06 22:37:01 +0100

    Add integration tests for non-existent instances

--- test-int/model-apis/theatres-api.test.js
+++ test-int/model-apis/theatres-api.test.js
@@ -171,4 +171,67 @@ describe('Theatres API', () => {
 
 	});
 
+	describe('requests for instances that do not exist in database', () => {
+
+		const NON_EXISTENT_THEATRE_UUID = 'foobar';
+
+		describe('GET edit endpoint', () => {
+
+			it('responds with 404 Not Found error', async () => {
+
+				const response = await chai.request(app)
+					.get(`/theatres/${NON_EXISTENT_THEATRE_UUID}/edit`);
+
+				expect(response).to.have.status(404);
+				expect(response.text).to.equal('Not Found');
+
+			});
+
+		});
+
+		describe('PUT update endpoint', () => {
+
+			it('responds with 404 Not Found error', async () => {
+
+				const response = await chai.request(app)
+					.put(`/theatres/${NON_EXISTENT_THEATRE_UUID}`)
+					.send({ name: 'Almeida Theatre' });
+
+				expect(response).to.have.status(404);
+				expect(response.text).to.equal('Not Found');
+
+			});
+
+		});
+
+		describe('GET show endpoint', () => {
+
+			it('responds with 404 Not Found error', async () => {
+
+				const response = await chai.request(app)
+					.get(`/theatres/${NON_EXISTENT_THEATRE_UUID}`);
+
+				expect(response).to.have.status(404);
+				expect(response.text).to.equal('Not Found');
+
+			});
+
+		});
+
+		describe('DELETE delete endpoint', () => {
+
+			it('responds with 404 Not Found error', async () => {
+
+				const response = await chai.request(app)
+					.delete(`/theatres/${NON_EXISTENT_THEATRE_UUID}`);
+
+				expect(response).to.have.status(404);
+				expect(response.text).to.equal('Not Found');
+
+			});
+
+		});
+
+	});
+
 });
