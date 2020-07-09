ff5985184980497f61a9640976cd98016a7a6fc7
Andy Gout <andygout@users.noreply.github.com>
2020-07-09 21:12:45 +0100

    Add integration tests for non-existent instances (#153)

--- test-int/model-apis/people-api.test.js
+++ test-int/model-apis/people-api.test.js
@@ -171,4 +171,67 @@ describe('People API', () => {
 
 	});
 
+	describe('requests for instances that do not exist in database', () => {
+
+		const NON_EXISTENT_PERSON_UUID = 'foobar';
+
+		describe('GET edit endpoint', () => {
+
+			it('responds with 404 Not Found error', async () => {
+
+				const response = await chai.request(app)
+					.get(`/people/${NON_EXISTENT_PERSON_UUID}/edit`);
+
+				expect(response).to.have.status(404);
+				expect(response.text).to.equal('Not Found');
+
+			});
+
+		});
+
+		describe('PUT update endpoint', () => {
+
+			it('responds with 404 Not Found error', async () => {
+
+				const response = await chai.request(app)
+					.put(`/people/${NON_EXISTENT_PERSON_UUID}`)
+					.send({ name: 'Patrick Stewart' });
+
+				expect(response).to.have.status(404);
+				expect(response.text).to.equal('Not Found');
+
+			});
+
+		});
+
+		describe('GET show endpoint', () => {
+
+			it('responds with 404 Not Found error', async () => {
+
+				const response = await chai.request(app)
+					.get(`/people/${NON_EXISTENT_PERSON_UUID}`);
+
+				expect(response).to.have.status(404);
+				expect(response.text).to.equal('Not Found');
+
+			});
+
+		});
+
+		describe('DELETE delete endpoint', () => {
+
+			it('responds with 404 Not Found error', async () => {
+
+				const response = await chai.request(app)
+					.delete(`/people/${NON_EXISTENT_PERSON_UUID}`);
+
+				expect(response).to.have.status(404);
+				expect(response.text).to.equal('Not Found');
+
+			});
+
+		});
+
+	});
+
 });
--- test-int/model-apis/playtexts-api.test.js
+++ test-int/model-apis/playtexts-api.test.js
@@ -485,4 +485,67 @@ describe('Playtexts API', () => {
 
 	});
 
+	describe('requests for instances that do not exist in database', () => {
+
+		const NON_EXISTENT_PLAYTEXT_UUID = 'foobar';
+
+		describe('GET edit endpoint', () => {
+
+			it('responds with 404 Not Found error', async () => {
+
+				const response = await chai.request(app)
+					.get(`/playtexts/${NON_EXISTENT_PLAYTEXT_UUID}/edit`);
+
+				expect(response).to.have.status(404);
+				expect(response.text).to.equal('Not Found');
+
+			});
+
+		});
+
+		describe('PUT update endpoint', () => {
+
+			it('responds with 404 Not Found error', async () => {
+
+				const response = await chai.request(app)
+					.put(`/playtexts/${NON_EXISTENT_PLAYTEXT_UUID}`)
+					.send({ name: 'The Cherry Orchard' });
+
+				expect(response).to.have.status(404);
+				expect(response.text).to.equal('Not Found');
+
+			});
+
+		});
+
+		describe('GET show endpoint', () => {
+
+			it('responds with 404 Not Found error', async () => {
+
+				const response = await chai.request(app)
+					.get(`/playtexts/${NON_EXISTENT_PLAYTEXT_UUID}`);
+
+				expect(response).to.have.status(404);
+				expect(response.text).to.equal('Not Found');
+
+			});
+
+		});
+
+		describe('DELETE delete endpoint', () => {
+
+			it('responds with 404 Not Found error', async () => {
+
+				const response = await chai.request(app)
+					.delete(`/playtexts/${NON_EXISTENT_PLAYTEXT_UUID}`);
+
+				expect(response).to.have.status(404);
+				expect(response.text).to.equal('Not Found');
+
+			});
+
+		});
+
+	});
+
 });
