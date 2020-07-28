83729daab6721288da4eee7cb73e6548d371f406
Andy Gout <andygout@users.noreply.github.com>
2020-07-28 21:16:18 +0100

    Rename integration tests -> end-to-end tests (#167)

--- test-e2e/mocha.env.js
+++ test-e2e/mocha.env.js
@@ -4,4 +4,4 @@ process.env.DATABASE_URL = 'bolt://0.0.0.0:7687';
 process.env.DATABASE_USERNAME = null;
 // Docker-served Neo4j is configured in `docker/docker-compose.yml` to require no authorisation: `NEO4J_AUTH=none`.
 process.env.DATABASE_PASSWORD = null;
-process.env.NODE_ENV = 'int-test';
+process.env.NODE_ENV = 'e2e-test';
