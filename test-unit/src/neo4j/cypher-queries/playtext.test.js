94381ba69b3916aad1b0c92e3fc338fc42455e4d
Andy Gout <andygout@users.noreply.github.com>
2020-08-14 15:59:20 +0100

    Add unit tests for Playtext model Cypher queries (#189)

---
+++ test-unit/src/neo4j/cypher-queries/playtext.test.js
@@ -0,0 +1,94 @@
+import { expect } from 'chai';
+
+import * as cypherQueriesPlaytext from '../../../../src/neo4j/cypher-queries/playtext';
+import removeWhitespace from '../../../test-helpers/remove-whitespace';
+
+describe('Cypher Queries Playtext module', () => {
+
+	describe('getCreateQuery function', () => {
+
+		it('returns requisite query', () => {
+
+			const result = cypherQueriesPlaytext.getCreateQuery();
+			expect(removeWhitespace(result)).to.equal(removeWhitespace(`
+				CREATE (playtext:Playtext { uuid: $uuid, name: $name })
+
+				FOREACH (characterParam IN $characters |
+					MERGE (character:Character { name: characterParam.name })
+					ON CREATE SET character.uuid = characterParam.uuid
+					CREATE (playtext)-[:INCLUDES_CHARACTER { position: characterParam.position }]->(character)
+				)
+
+				WITH playtext
+
+				MATCH (playtext:Playtext { uuid: $uuid })
+
+				OPTIONAL MATCH (playtext)-[characterRel:INCLUDES_CHARACTER]->(character:Character)
+
+				WITH playtext, characterRel, character
+					ORDER BY characterRel.position
+
+				RETURN
+					'playtext' AS model,
+					playtext.uuid AS uuid,
+					playtext.name AS name,
+					COLLECT(
+						CASE WHEN character IS NULL
+							THEN null
+							ELSE { name: character.name }
+						END
+					) + [{ name: '' }] AS characters
+			`));
+
+		});
+
+	});
+
+	describe('getUpdateQuery function', () => {
+
+		it('returns requisite query', () => {
+
+			const result = cypherQueriesPlaytext.getUpdateQuery();
+			expect(removeWhitespace(result)).to.equal(removeWhitespace(`
+				MATCH (playtext:Playtext { uuid: $uuid })
+
+				OPTIONAL MATCH (playtext)-[relationship:INCLUDES_CHARACTER]->(:Character)
+
+				DELETE relationship
+
+				WITH DISTINCT playtext
+
+				SET playtext.name = $name
+
+				FOREACH (characterParam IN $characters |
+					MERGE (character:Character { name: characterParam.name })
+					ON CREATE SET character.uuid = characterParam.uuid
+					CREATE (playtext)-[:INCLUDES_CHARACTER { position: characterParam.position }]->(character)
+				)
+
+				WITH playtext
+
+				MATCH (playtext:Playtext { uuid: $uuid })
+
+				OPTIONAL MATCH (playtext)-[characterRel:INCLUDES_CHARACTER]->(character:Character)
+
+				WITH playtext, characterRel, character
+					ORDER BY characterRel.position
+
+				RETURN
+					'playtext' AS model,
+					playtext.uuid AS uuid,
+					playtext.name AS name,
+					COLLECT(
+						CASE WHEN character IS NULL
+							THEN null
+							ELSE { name: character.name }
+						END
+					) + [{ name: '' }] AS characters
+			`));
+
+		});
+
+	});
+
+});
