e78c6f28e7863cda85bd57744bf7e9ea83f3d81d
andygout <andygout@hotmail.co.uk>
2021-04-20 22:02:07 +0100

    Revise Cypher queries unit tests

--- test-unit/src/neo4j/cypher-queries/material.test.js
+++ test-unit/src/neo4j/cypher-queries/material.test.js
@@ -10,215 +10,18 @@ describe('Cypher Queries Material module', () => {
 		it('returns requisite query', () => {
 
 			const result = cypherQueriesMaterial.getCreateQuery();
-			expect(removeExcessWhitespace(result)).to.equal(removeExcessWhitespace(`
+
+			const compactedResult = removeExcessWhitespace(result);
+
+			const startSegment = removeExcessWhitespace(`
 				CREATE (material:Material { uuid: $uuid, name: $name, differentiator: $differentiator, format: $format })
+			`);
 
-				WITH material
+			const middleSegment = removeExcessWhitespace(`
+				CREATE (material)-[:SUBSEQUENT_VERSION_OF]->(originalVersionMaterial)
+			`);
 
-				OPTIONAL MATCH (existingOriginalVersionMaterial:Material { name: $originalVersionMaterial.name })
-					WHERE
-						(
-							$originalVersionMaterial.differentiator IS NULL AND
-							existingOriginalVersionMaterial.differentiator IS NULL
-						) OR
-						$originalVersionMaterial.differentiator = existingOriginalVersionMaterial.differentiator
-
-				FOREACH (item IN CASE $originalVersionMaterial.name WHEN NULL THEN [] ELSE [1] END |
-					MERGE (originalVersionMaterial:Material {
-						uuid: COALESCE(existingOriginalVersionMaterial.uuid, $originalVersionMaterial.uuid),
-						name: $originalVersionMaterial.name
-					})
-						ON CREATE SET originalVersionMaterial.differentiator = $originalVersionMaterial.differentiator
-
-					CREATE (material)-[:SUBSEQUENT_VERSION_OF]->(originalVersionMaterial)
-				)
-
-				WITH material
-
-				UNWIND (CASE $writingCredits WHEN [] THEN [{ entities: [] }] ELSE $writingCredits END) AS writingCredit
-
-					UNWIND
-						CASE SIZE([entity IN writingCredit.entities WHERE entity.model = 'person']) WHEN 0
-							THEN [null]
-							ELSE [entity IN writingCredit.entities WHERE entity.model = 'person']
-						END AS writingPersonParam
-
-						OPTIONAL MATCH (existingWritingPerson:Person { name: writingPersonParam.name })
-							WHERE
-								(writingPersonParam.differentiator IS NULL AND existingWritingPerson.differentiator IS NULL) OR
-								writingPersonParam.differentiator = existingWritingPerson.differentiator
-
-						FOREACH (item IN CASE writingPersonParam WHEN NULL THEN [] ELSE [1] END |
-							MERGE (writingPerson:Person {
-								uuid: COALESCE(existingWritingPerson.uuid, writingPersonParam.uuid),
-								name: writingPersonParam.name
-							})
-								ON CREATE SET writingPerson.differentiator = writingPersonParam.differentiator
-
-							CREATE (material)-
-								[:HAS_WRITING_ENTITY {
-									creditPosition: writingCredit.position,
-									entityPosition: writingPersonParam.position,
-									credit: writingCredit.name,
-									creditType: writingCredit.creditType
-								}]->(writingPerson)
-						)
-
-					WITH DISTINCT material, writingCredit
-
-					UNWIND
-						CASE SIZE([entity IN writingCredit.entities WHERE entity.model = 'company']) WHEN 0
-							THEN [null]
-							ELSE [entity IN writingCredit.entities WHERE entity.model = 'company']
-						END AS writingCompanyParam
-
-						OPTIONAL MATCH (existingWritingCompany:Company { name: writingCompanyParam.name })
-							WHERE
-								(
-									writingCompanyParam.differentiator IS NULL AND
-									existingWritingCompany.differentiator IS NULL
-								) OR
-								writingCompanyParam.differentiator = existingWritingCompany.differentiator
-
-						FOREACH (item IN CASE writingCompanyParam WHEN NULL THEN [] ELSE [1] END |
-							MERGE (writingCompany:Company {
-								uuid: COALESCE(existingWritingCompany.uuid, writingCompanyParam.uuid),
-								name: writingCompanyParam.name
-							})
-								ON CREATE SET writingCompany.differentiator = writingCompanyParam.differentiator
-
-							CREATE (material)-
-								[:HAS_WRITING_ENTITY {
-									creditPosition: writingCredit.position,
-									entityPosition: writingCompanyParam.position,
-									credit: writingCredit.name,
-									creditType: writingCredit.creditType
-								}]->(writingCompany)
-						)
-
-					WITH DISTINCT material, writingCredit
-
-					UNWIND
-						CASE SIZE([entity IN writingCredit.entities WHERE entity.model = 'material']) WHEN 0
-							THEN [null]
-							ELSE [entity IN writingCredit.entities WHERE entity.model = 'material']
-						END AS sourceMaterialParam
-
-						OPTIONAL MATCH (existingSourceMaterial:Material { name: sourceMaterialParam.name })
-							WHERE
-								(
-									sourceMaterialParam.differentiator IS NULL AND
-									existingSourceMaterial.differentiator IS NULL
-								) OR
-								sourceMaterialParam.differentiator = existingSourceMaterial.differentiator
-
-						FOREACH (item IN CASE sourceMaterialParam WHEN NULL THEN [] ELSE [1] END |
-							MERGE (sourceMaterial:Material {
-								uuid: COALESCE(existingSourceMaterial.uuid, sourceMaterialParam.uuid),
-								name: sourceMaterialParam.name
-							})
-								ON CREATE SET sourceMaterial.differentiator = sourceMaterialParam.differentiator
-
-							CREATE (material)-
-								[:USES_SOURCE_MATERIAL {
-									creditPosition: writingCredit.position,
-									entityPosition: sourceMaterialParam.position,
-									credit: writingCredit.name
-								}]->(sourceMaterial)
-						)
-
-				WITH DISTINCT material
-
-				UNWIND (CASE $characterGroups WHEN [] THEN [{ characters: [] }] ELSE $characterGroups END) AS characterGroup
-
-					UNWIND (CASE characterGroup.characters WHEN []
-						THEN [null]
-						ELSE characterGroup.characters
-					END) AS characterParam
-
-						OPTIONAL MATCH (existingCharacter:Character {
-							name: COALESCE(characterParam.underlyingName, characterParam.name)
-						})
-							WHERE
-								(characterParam.differentiator IS NULL AND existingCharacter.differentiator IS NULL) OR
-								characterParam.differentiator = existingCharacter.differentiator
-
-						FOREACH (item IN CASE characterParam WHEN NULL THEN [] ELSE [1] END |
-							MERGE (character:Character {
-								uuid: COALESCE(existingCharacter.uuid, characterParam.uuid),
-								name: COALESCE(characterParam.underlyingName, characterParam.name)
-							})
-								ON CREATE SET character.differentiator = characterParam.differentiator
-
-							CREATE (material)-
-								[:HAS_CHARACTER {
-									groupPosition: characterGroup.position,
-									characterPosition: characterParam.position,
-									displayName: CASE characterParam.underlyingName WHEN NULL
-										THEN null
-										ELSE characterParam.name
-									END,
-									qualifier: characterParam.qualifier,
-									group: characterGroup.name
-								}]->(character)
-						)
-
-				WITH DISTINCT material
-
-				MATCH (material:Material { uuid: $uuid })
-
-				OPTIONAL MATCH (material)-[:SUBSEQUENT_VERSION_OF]->(originalVersionMaterial:Material)
-
-				OPTIONAL MATCH (material)-[entityRel:HAS_WRITING_ENTITY|USES_SOURCE_MATERIAL]->(entity)
-					WHERE entity:Person OR entity:Company OR entity:Material
-
-				WITH material, originalVersionMaterial, entityRel, entity
-					ORDER BY entityRel.creditPosition, entityRel.entityPosition
-
-				WITH
-					material,
-					originalVersionMaterial,
-					entityRel.credit AS writingCreditName,
-					entityRel.creditType AS writingCreditType,
-					COLLECT(
-						CASE entity WHEN NULL
-							THEN null
-							ELSE entity { model: TOLOWER(HEAD(LABELS(entity))), .name, .differentiator }
-						END
-					) + [{}] AS entities
-
-				WITH material, originalVersionMaterial,
-					COLLECT(
-						CASE WHEN writingCreditName IS NULL AND SIZE(entities) = 1
-							THEN null
-							ELSE {
-								model: 'writingCredit',
-								name: writingCreditName,
-								creditType: writingCreditType,
-								entities: entities
-							}
-						END
-					) + [{ entities: [{}] }] AS writingCredits
-
-				OPTIONAL MATCH (material)-[characterRel:HAS_CHARACTER]->(character:Character)
-
-				WITH material, originalVersionMaterial, writingCredits, characterRel, character
-					ORDER BY characterRel.groupPosition, characterRel.characterPosition
-
-				WITH material, originalVersionMaterial, writingCredits, characterRel.group AS characterGroupName,
-					COLLECT(
-						CASE character WHEN NULL
-							THEN null
-							ELSE character {
-								name: COALESCE(characterRel.displayName, character.name),
-								underlyingName: CASE characterRel.displayName WHEN NULL THEN null ELSE character.name END,
-								.differentiator,
-								qualifier: characterRel.qualifier,
-								group: characterRel.group
-							}
-						END
-					) + [{}] AS characters
-
+			const endSegment = removeExcessWhitespace(`
 				RETURN
 					'material' AS model,
 					material.uuid AS uuid,
@@ -236,7 +39,11 @@ describe('Cypher Queries Material module', () => {
 							ELSE { model: 'characterGroup', name: characterGroupName, characters: characters }
 						END
 					) + [{ characters: [{}] }] AS characterGroups
-			`));
+			`);
+
+			expect(compactedResult.startsWith(startSegment)).to.be.true;
+			expect(compactedResult.includes(middleSegment)).to.be.true;
+			expect(compactedResult.endsWith(endSegment)).to.be.true;
 
 		});
 
@@ -247,7 +54,10 @@ describe('Cypher Queries Material module', () => {
 		it('returns requisite query', () => {
 
 			const result = cypherQueriesMaterial.getUpdateQuery();
-			expect(removeExcessWhitespace(result)).to.equal(removeExcessWhitespace(`
+
+			const compactedResult = removeExcessWhitespace(result);
+
+			const startSegment = removeExcessWhitespace(`
 				MATCH (material:Material { uuid: $uuid })
 
 				OPTIONAL MATCH (material)-[originalVersionMaterialRel:SUBSEQUENT_VERSION_OF]->(:Material)
@@ -279,213 +89,13 @@ describe('Cypher Queries Material module', () => {
 					material.name = $name,
 					material.differentiator = $differentiator,
 					material.format = $format
+			`);
 
-				WITH material
+			const middleSegment = removeExcessWhitespace(`
+				CREATE (material)-[:SUBSEQUENT_VERSION_OF]->(originalVersionMaterial)
+			`);
 
-				OPTIONAL MATCH (existingOriginalVersionMaterial:Material { name: $originalVersionMaterial.name })
-					WHERE
-						(
-							$originalVersionMaterial.differentiator IS NULL AND
-							existingOriginalVersionMaterial.differentiator IS NULL
-						) OR
-						$originalVersionMaterial.differentiator = existingOriginalVersionMaterial.differentiator
-
-				FOREACH (item IN CASE $originalVersionMaterial.name WHEN NULL THEN [] ELSE [1] END |
-					MERGE (originalVersionMaterial:Material {
-						uuid: COALESCE(existingOriginalVersionMaterial.uuid, $originalVersionMaterial.uuid),
-						name: $originalVersionMaterial.name
-					})
-						ON CREATE SET originalVersionMaterial.differentiator = $originalVersionMaterial.differentiator
-
-					CREATE (material)-[:SUBSEQUENT_VERSION_OF]->(originalVersionMaterial)
-				)
-
-				WITH material
-
-				UNWIND (CASE $writingCredits WHEN [] THEN [{ entities: [] }] ELSE $writingCredits END) AS writingCredit
-
-					UNWIND
-						CASE SIZE([entity IN writingCredit.entities WHERE entity.model = 'person']) WHEN 0
-							THEN [null]
-							ELSE [entity IN writingCredit.entities WHERE entity.model = 'person']
-						END AS writingPersonParam
-
-						OPTIONAL MATCH (existingWritingPerson:Person { name: writingPersonParam.name })
-							WHERE
-								(writingPersonParam.differentiator IS NULL AND existingWritingPerson.differentiator IS NULL) OR
-								writingPersonParam.differentiator = existingWritingPerson.differentiator
-
-						FOREACH (item IN CASE writingPersonParam WHEN NULL THEN [] ELSE [1] END |
-							MERGE (writingPerson:Person {
-								uuid: COALESCE(existingWritingPerson.uuid, writingPersonParam.uuid),
-								name: writingPersonParam.name
-							})
-								ON CREATE SET writingPerson.differentiator = writingPersonParam.differentiator
-
-							CREATE (material)-
-								[:HAS_WRITING_ENTITY {
-									creditPosition: writingCredit.position,
-									entityPosition: writingPersonParam.position,
-									credit: writingCredit.name,
-									creditType: writingCredit.creditType
-								}]->(writingPerson)
-						)
-
-					WITH DISTINCT material, writingCredit
-
-					UNWIND
-						CASE SIZE([entity IN writingCredit.entities WHERE entity.model = 'company']) WHEN 0
-							THEN [null]
-							ELSE [entity IN writingCredit.entities WHERE entity.model = 'company']
-						END AS writingCompanyParam
-
-						OPTIONAL MATCH (existingWritingCompany:Company { name: writingCompanyParam.name })
-							WHERE
-								(
-									writingCompanyParam.differentiator IS NULL AND
-									existingWritingCompany.differentiator IS NULL
-								) OR
-								writingCompanyParam.differentiator = existingWritingCompany.differentiator
-
-						FOREACH (item IN CASE writingCompanyParam WHEN NULL THEN [] ELSE [1] END |
-							MERGE (writingCompany:Company {
-								uuid: COALESCE(existingWritingCompany.uuid, writingCompanyParam.uuid),
-								name: writingCompanyParam.name
-							})
-								ON CREATE SET writingCompany.differentiator = writingCompanyParam.differentiator
-
-							CREATE (material)-
-								[:HAS_WRITING_ENTITY {
-									creditPosition: writingCredit.position,
-									entityPosition: writingCompanyParam.position,
-									credit: writingCredit.name,
-									creditType: writingCredit.creditType
-								}]->(writingCompany)
-						)
-
-					WITH DISTINCT material, writingCredit
-
-					UNWIND
-						CASE SIZE([entity IN writingCredit.entities WHERE entity.model = 'material']) WHEN 0
-							THEN [null]
-							ELSE [entity IN writingCredit.entities WHERE entity.model = 'material']
-						END AS sourceMaterialParam
-
-						OPTIONAL MATCH (existingSourceMaterial:Material { name: sourceMaterialParam.name })
-							WHERE
-								(
-									sourceMaterialParam.differentiator IS NULL AND
-									existingSourceMaterial.differentiator IS NULL
-								) OR
-								sourceMaterialParam.differentiator = existingSourceMaterial.differentiator
-
-						FOREACH (item IN CASE sourceMaterialParam WHEN NULL THEN [] ELSE [1] END |
-							MERGE (sourceMaterial:Material {
-								uuid: COALESCE(existingSourceMaterial.uuid, sourceMaterialParam.uuid),
-								name: sourceMaterialParam.name
-							})
-								ON CREATE SET sourceMaterial.differentiator = sourceMaterialParam.differentiator
-
-							CREATE (material)-
-								[:USES_SOURCE_MATERIAL {
-									creditPosition: writingCredit.position,
-									entityPosition: sourceMaterialParam.position,
-									credit: writingCredit.name
-								}]->(sourceMaterial)
-						)
-
-				WITH DISTINCT material
-
-				UNWIND (CASE $characterGroups WHEN [] THEN [{ characters: [] }] ELSE $characterGroups END) AS characterGroup
-
-					UNWIND (CASE characterGroup.characters WHEN []
-						THEN [null]
-						ELSE characterGroup.characters
-					END) AS characterParam
-
-						OPTIONAL MATCH (existingCharacter:Character {
-							name: COALESCE(characterParam.underlyingName, characterParam.name)
-						})
-							WHERE
-								(characterParam.differentiator IS NULL AND existingCharacter.differentiator IS NULL) OR
-								characterParam.differentiator = existingCharacter.differentiator
-
-						FOREACH (item IN CASE characterParam WHEN NULL THEN [] ELSE [1] END |
-							MERGE (character:Character {
-								uuid: COALESCE(existingCharacter.uuid, characterParam.uuid),
-								name: COALESCE(characterParam.underlyingName, characterParam.name)
-							})
-								ON CREATE SET character.differentiator = characterParam.differentiator
-
-							CREATE (material)-
-								[:HAS_CHARACTER {
-									groupPosition: characterGroup.position,
-									characterPosition: characterParam.position,
-									displayName: CASE characterParam.underlyingName WHEN NULL
-										THEN null
-										ELSE characterParam.name
-									END,
-									qualifier: characterParam.qualifier,
-									group: characterGroup.name
-								}]->(character)
-						)
-
-				WITH DISTINCT material
-
-				MATCH (material:Material { uuid: $uuid })
-
-				OPTIONAL MATCH (material)-[:SUBSEQUENT_VERSION_OF]->(originalVersionMaterial:Material)
-
-				OPTIONAL MATCH (material)-[entityRel:HAS_WRITING_ENTITY|USES_SOURCE_MATERIAL]->(entity)
-					WHERE entity:Person OR entity:Company OR entity:Material
-
-				WITH material, originalVersionMaterial, entityRel, entity
-					ORDER BY entityRel.creditPosition, entityRel.entityPosition
-
-				WITH
-					material,
-					originalVersionMaterial,
-					entityRel.credit AS writingCreditName,
-					entityRel.creditType AS writingCreditType,
-					COLLECT(
-						CASE entity WHEN NULL
-							THEN null
-							ELSE entity { model: TOLOWER(HEAD(LABELS(entity))), .name, .differentiator }
-						END
-					) + [{}] AS entities
-
-				WITH material, originalVersionMaterial,
-					COLLECT(
-						CASE WHEN writingCreditName IS NULL AND SIZE(entities) = 1
-							THEN null
-							ELSE {
-								model: 'writingCredit',
-								name: writingCreditName,
-								creditType: writingCreditType,
-								entities: entities
-							}
-						END
-					) + [{ entities: [{}] }] AS writingCredits
-
-				OPTIONAL MATCH (material)-[characterRel:HAS_CHARACTER]->(character:Character)
-
-				WITH material, originalVersionMaterial, writingCredits, characterRel, character
-					ORDER BY characterRel.groupPosition, characterRel.characterPosition
-
-				WITH material, originalVersionMaterial, writingCredits, characterRel.group AS characterGroupName,
-					COLLECT(
-						CASE character WHEN NULL
-							THEN null
-							ELSE character {
-								name: COALESCE(characterRel.displayName, character.name),
-								underlyingName: CASE characterRel.displayName WHEN NULL THEN null ELSE character.name END,
-								.differentiator,
-								qualifier: characterRel.qualifier,
-								group: characterRel.group
-							}
-						END
-					) + [{}] AS characters
-
+			const endSegment = removeExcessWhitespace(`
 				RETURN
 					'material' AS model,
 					material.uuid AS uuid,
@@ -503,7 +113,11 @@ describe('Cypher Queries Material module', () => {
 							ELSE { model: 'characterGroup', name: characterGroupName, characters: characters }
 						END
 					) + [{ characters: [{}] }] AS characterGroups
-			`));
+			`);
+
+			expect(compactedResult.startsWith(startSegment)).to.be.true;
+			expect(compactedResult.includes(middleSegment)).to.be.true;
+			expect(compactedResult.endsWith(endSegment)).to.be.true;
 
 		});
 
