2b122f66c1b6266bc507595184796cb4315dbdab
Andy Gout <andygout@hotmail.co.uk>
2022-09-28 16:54:28 +0100

    Upgrade mocha ^3.2.0 -> ^10.0.0

--- test-e2e/setup.js
+++ test-e2e/setup.js
@@ -1,10 +1,17 @@
 import createNeo4jConstraints from '../src/neo4j/create-constraints';
 import createNeo4jIndexes from '../src/neo4j/create-indexes';
+import { shutDown } from '../src/app';
 
 before(async () => {
 
 	await createNeo4jConstraints();
 
 	await createNeo4jIndexes();
+
+});
+
+after(() => {
+
+	shutDown();
 
 });
