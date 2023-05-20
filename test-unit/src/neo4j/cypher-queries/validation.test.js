a1a11f543ae173f84dc391ad7f7f5cd3ad5e26b1
Andy Gout <andygout@hotmail.co.uk>
2023-05-20 17:16:07 +0100

    Add Cypher queries validation queries unit tests

--- test-unit/src/neo4j/cypher-queries/validation.test.js
+++ test-unit/src/neo4j/cypher-queries/validation.test.js
@@ -5,21 +5,6 @@ import removeExcessWhitespace from '../../../test-helpers/remove-excess-whitespa
 
 describe('Cypher Queries Validation module', () => {
 
-	describe('getExistenceQuery function', () => {
-
-		it('returns requisite query', () => {
-
-			const result = cypherQueriesValidation.getExistenceQuery('VENUE');
-			expect(removeExcessWhitespace(result)).to.equal(removeExcessWhitespace(`
-				MATCH (n:Venue { uuid: $uuid })
-
-				RETURN n
-			`));
-
-		});
-
-	});
-
 	describe('getDuplicateRecordCountQuery function', () => {
 
 		it('returns requisite query', () => {
@@ -38,6 +23,93 @@ describe('Cypher Queries Validation module', () => {
 						)
 
 				RETURN SIGN(COUNT(n)) AS duplicateRecordCount
+			`));
+
+		});
+
+	});
+
+	describe('getExistenceQuery function', () => {
+
+		it('returns requisite query', () => {
+
+			const result = cypherQueriesValidation.getExistenceQuery('VENUE');
+			expect(removeExcessWhitespace(result)).to.equal(removeExcessWhitespace(`
+				MATCH (n:Venue { uuid: $uuid })
+
+				RETURN n
+			`));
+
+		});
+
+	});
+
+	describe('getSubMaterialChecksQuery function', () => {
+
+		it('returns requisite query', () => {
+
+			const result = cypherQueriesValidation.getSubMaterialChecksQuery();
+			expect(removeExcessWhitespace(result)).to.equal(removeExcessWhitespace(`
+				MATCH (m:Material { name: $name })
+					WHERE
+						(
+							($differentiator IS NULL AND m.differentiator IS NULL) OR
+							$differentiator = m.differentiator
+						)
+
+				OPTIONAL MATCH (subjectMaterial:Material { uuid: $subjectMaterialUuid })
+
+				OPTIONAL MATCH (m)<-[surMaterialRel:HAS_SUB_MATERIAL]-(:Material)
+
+				OPTIONAL MATCH (m)
+					-[:HAS_SUB_MATERIAL]->(:Material)
+					-[subSubMaterialRel:HAS_SUB_MATERIAL]->(:Material)
+
+				OPTIONAL MATCH (m)-[subjectMaterialSubMaterialRel:HAS_SUB_MATERIAL]->(subjectMaterial)
+
+				OPTIONAL MATCH (subjectMaterial)
+					<-[:HAS_SUB_MATERIAL]-(:Material)
+					<-[surSurMaterialRel:HAS_SUB_MATERIAL]-(:Material)
+
+				RETURN
+					TOBOOLEAN(COUNT(surMaterialRel)) AS isAssignedToSurMaterial,
+					TOBOOLEAN(COUNT(subSubMaterialRel)) AS isSurSurMaterial,
+					TOBOOLEAN(COUNT(subjectMaterialSubMaterialRel)) AS isSurMaterialOfSubjectMaterial,
+					TOBOOLEAN(COUNT(surSurMaterialRel)) AS isSubjectMaterialASubSubMaterial
+			`));
+
+		});
+
+	});
+
+	describe('getSubProductionChecksQuery function', () => {
+
+		it('returns requisite query', () => {
+
+			const result = cypherQueriesValidation.getSubProductionChecksQuery('VENUE');
+			expect(removeExcessWhitespace(result)).to.equal(removeExcessWhitespace(`
+				MATCH (p:Production { uuid: $uuid })
+
+				OPTIONAL MATCH (subjectProduction:Production { uuid: $subjectProductionUuid })
+
+				OPTIONAL MATCH (p)<-[surProductionRel:HAS_SUB_PRODUCTION]-(:Production)
+
+				OPTIONAL MATCH (p)
+					-[:HAS_SUB_PRODUCTION]->(:Production)
+					-[subSubProductionRel:HAS_SUB_PRODUCTION]->(:Production)
+
+				OPTIONAL MATCH (p)-[subjectProductionSubProductionRel:HAS_SUB_PRODUCTION]->(subjectProduction)
+
+				OPTIONAL MATCH (subjectProduction)
+					<-[:HAS_SUB_PRODUCTION]-(:Production)
+					<-[surSurProductionRel:HAS_SUB_PRODUCTION]-(:Production)
+
+				RETURN
+					TOBOOLEAN(COUNT(p)) AS exists,
+					TOBOOLEAN(COUNT(surProductionRel)) AS isAssignedToSurProduction,
+					TOBOOLEAN(COUNT(subSubProductionRel)) AS isSurSurProduction,
+					TOBOOLEAN(COUNT(subjectProductionSubProductionRel)) AS isSurProductionOfSubjectProduction,
+					TOBOOLEAN(COUNT(surSurProductionRel)) AS isSubjectProductionASubSubProduction
 			`));
 
 		});
