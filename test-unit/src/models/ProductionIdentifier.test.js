626370892410b84a3cd811285ac160e8bf5acf7d
Andy Gout <andygout@users.noreply.github.com>
2023-06-03 16:45:29 +0100

    Add ProductionIdentifier class constructor unit tests (#548)

--- test-unit/src/models/ProductionIdentifier.test.js
+++ test-unit/src/models/ProductionIdentifier.test.js
@@ -1,8 +1,52 @@
+import { expect } from 'chai';
 import { assert, spy } from 'sinon';
 
 import { ProductionIdentifier } from '../../../src/models';
 
 describe('ProductionIdentifier model', () => {
+
+	describe('constructor method', () => {
+
+		describe('uuid property', () => {
+
+			it('assigns empty string if absent from props', () => {
+
+				const instance = new ProductionIdentifier({});
+				expect(instance.uuid).to.equal('');
+
+			});
+
+			it('assigns empty string if included in props but value is empty string', () => {
+
+				const instance = new ProductionIdentifier({ uuid: '' });
+				expect(instance.uuid).to.equal('');
+
+			});
+
+			it('assigns empty string if included in props but value is whitespace-only string', () => {
+
+				const instance = new ProductionIdentifier({ uuid: ' ' });
+				expect(instance.uuid).to.equal('');
+
+			});
+
+			it('assigns value if included in props and is string with length', () => {
+
+				const instance = new ProductionIdentifier({ uuid: 'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx' });
+				expect(instance.uuid).to.equal('xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx');
+
+			});
+
+			it('trims value before assigning', () => {
+
+				const instance = new ProductionIdentifier({ uuid: ' xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx ' });
+				expect(instance.uuid).to.equal('xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx');
+
+			});
+
+		});
+
+	});
 
 	describe('validateUuid method', () => {
 
