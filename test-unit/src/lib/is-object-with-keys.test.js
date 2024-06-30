c7b5795bb79a354d4195a0d3b2991088fbda9f89
Andy Gout <andygout@hotmail.co.uk>
2024-06-30 15:19:03 +0100

    Use Node.js native test runner

--- test-unit/src/lib/is-object-with-keys.test.js
+++ test-unit/src/lib/is-object-with-keys.test.js
@@ -1,4 +1,5 @@
-import { expect } from 'chai';
+import { describe, it } from 'node:test';
+import assert from 'node:assert/strict';
 
 import isObjectWithKeys from '../../../src/lib/is-object-with-keys.js';
 
@@ -6,43 +7,43 @@ describe('Is Object With Keys module', () => {
 
 	it('considers object with keys as valid object', () => {
 
-		expect(isObjectWithKeys({ key: 'value' })).to.be.true;
+		assert.equal(isObjectWithKeys({ key: 'value' }), true);
 
 	});
 
 	it('will not consider null (which is type of object) as valid object', () => {
 
-		expect(isObjectWithKeys(null)).to.be.false;
+		assert.equal(isObjectWithKeys(null), false);
 
 	});
 
 	it('will not consider empty array (which is type of object) as valid object', () => {
 
-		expect(isObjectWithKeys([])).to.be.false;
+		assert.equal(isObjectWithKeys([]), false);
 
 	});
 
 	it('will not consider populated array (which is type of object) as valid object', () => {
 
-		expect(isObjectWithKeys([1, 2, 3])).to.be.false;
+		assert.equal(isObjectWithKeys([1, 2, 3]), false);
 
 	});
 
 	it('will not consider string type as valid object', () => {
 
-		expect(isObjectWithKeys('string')).to.be.false;
+		assert.equal(isObjectWithKeys('string'), false);
 
 	});
 
 	it('will not consider number type as valid object', () => {
 
-		expect(isObjectWithKeys(123)).to.be.false;
+		assert.equal(isObjectWithKeys(123), false);
 
 	});
 
 	it('will not consider empty object (i.e. no keys) as valid object', () => {
 
-		expect(isObjectWithKeys({})).to.be.false;
+		assert.equal(isObjectWithKeys({}), false);
 
 	});
 
