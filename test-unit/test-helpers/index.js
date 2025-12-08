bf9b25e9fced7f09b059000d5c6530fd3d353709
Andy Gout <andygout@users.noreply.github.com>
2025-12-08 19:11:26 +0000

    Add index for unit test helpers (#710)

---
+++ test-unit/test-helpers/index.js
@@ -0,0 +1,7 @@
+import applyModelGetter from './apply-model-getter.js';
+import removeExcessWhitespace from './remove-excess-whitespace.js';
+
+export {
+	applyModelGetter,
+	removeExcessWhitespace
+};
