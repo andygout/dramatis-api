34aa625345df224d3955fb042a4c4962c6dd3822
Andy Gout <andygout@users.noreply.github.com>
2026-09-14 22:39:01 +0100

    Qualify references to Neo4j range indexes (#770)

--- test-e2e/global-setup.js
+++ test-e2e/global-setup.js
@@ -1,12 +1,12 @@
 import createNeo4jConstraints from '../src/neo4j/create-constraints.js';
 import createNeo4jFullTextIndexes from '../src/neo4j/create-full-text-indexes.js';
-import createNeo4jIndexes from '../src/neo4j/create-indexes.js';
+import createNeo4jRangeIndexes from '../src/neo4j/create-range-indexes.js';
 import { getDriver as getNeo4jDriver } from '../src/neo4j/get-driver.js';
 
 export async function globalSetup() {
 	await createNeo4jConstraints();
 
-	await createNeo4jIndexes();
+	await createNeo4jRangeIndexes();
 
 	await createNeo4jFullTextIndexes();
 }
